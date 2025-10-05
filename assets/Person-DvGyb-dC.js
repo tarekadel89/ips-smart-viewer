8b4dae4e7aab82f0d522fc3d1d9a4736711488c4
Tarek Soliman <tarekadel89@gmail.com>
2025-10-05 17:26:50 +1100

    Updates

--- assets/Person-DvGyb-dC.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./index-BX8N34_g.js";import{c as s}from"./Chip-DojAl99Q.js";const a=s(o.jsx("path",{d:"M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z"})),n=s(o.jsx("path",{d:"M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4m0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4"}));export{a as D,n as P};
